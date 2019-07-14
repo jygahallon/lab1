63729bdd4c76940dbd39242d92cf7da4aa4fe60f
Jamie Gahallon <jygahallon@student.apc.edu.ph>
2019-07-15 00:53:35 +0800

    jquery started

---
+++ static/js/own.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $('body').css('display', 'none');
+  $('body').fadeIn(1000);
+
+  $('.gimages')
+    .wrap('<span style="display:inline-block"></span>')
+    .css('display', 'block')
+    .parent()
+    .zoom();
+
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+      backToTop = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+          $('#back-to-top').addClass('show');
+        } else {
+          $('#back-to-top').removeClass('show');
+        }
+      };
+    backToTop();
+    $(window).on('scroll', function () {
+      backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
+    });
+  }
+
+});
